2b8660024902ba6a04c058efd0a454eec760fcfd
Grigory <g.usoltsev.worksrocket@gmail.com>
2023-04-16 21:30:45 +0500

    footer

--- src/js/components/smoothHeader/smoothHeader.js
+++ src/js/components/smoothHeader/smoothHeader.js
@@ -1,28 +1,28 @@
 
 
 function smoothHeader() {
-	const trigger = document.getElementById('triggerHeader').offsetTop;
-	// const preTrigger = (trigger / 10) * 5;
-	const header = document.querySelector('.headerSmooth'); 
-	const addClass = (item, cls) => {
-		 item.classList.add(cls)
-	}
+	// const trigger = document.getElementById('triggerHeader').offsetTop;
+	// // const preTrigger = (trigger / 10) * 5;
+	// const header = document.querySelector('.headerSmooth'); 
+	// const addClass = (item, cls) => {
+	// 	 item.classList.add(cls)
+	// }
 
-	const removeClass = (item, cls) => {
-		item.classList.remove(cls)
-	}
+	// const removeClass = (item, cls) => {
+	// 	item.classList.remove(cls)
+	// }
 
-	if (header) {
-		document.addEventListener('scroll', (el) => {
-			let scrollDistance = window.scrollY;
+	// if (header) {
+	// 	document.addEventListener('scroll', (el) => {
+	// 		let scrollDistance = window.scrollY;
 	 
-			if (scrollDistance >= trigger) {
-				addClass(header, 'header--fixed') 
-			} else {
-				removeClass(header, 'header--fixed')
-			}
-		})
-	}
+	// 		if (scrollDistance >= trigger) {
+	// 			addClass(header, 'header--fixed') 
+	// 		} else {
+	// 			removeClass(header, 'header--fixed')
+	// 		}
+	// 	})
+	// }
 
 	
 }
